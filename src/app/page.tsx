2c282b322c6e024078b1a7c750dbd6b8189e17d3
phong28zk <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-05-15 23:51:03 +0700

    Style: Restyle in StyledHeader components and restyle Images in gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,24 @@ export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   const images = await db.query.images.findMany({
-    orderBy: (model, {
-      desc
-    }) => desc(model.id),
+    orderBy: (model, { desc }) => desc(model.id),
   });
   console.log(images);
   return (
-    <div className="grid max-w-[1024px] grid-cols-12 gap-4 m-4">
+    <div className="m-4 grid max-w-[1024px] grid-cols-12 gap-4">
       {images.map((image, index) => (
-        <div key={image.id + "-" + index} className="col-span-3 max-h-64 max-w-64">
-          {image.name}
-          <Image src={image.url} alt="alt" width={256} height={256} />
+        <div
+          key={image.id + "-" + index}
+          className="col-span-3 max-h-64 max-w-64 overflow-hidden"
+        >
+          <Image
+            src={image.url}
+            title={image.name}
+            alt="alt"
+            width={256}
+            height={256}
+            className="group object-cover"
+          />
         </div>
       ))}
     </div>
