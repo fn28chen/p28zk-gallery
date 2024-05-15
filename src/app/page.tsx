f82fb6c405a8dbf9300e47239da6599b5d69f3c3
phong28zk <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-05-15 13:40:55 +0700

    Test: Test after setting Drizzle complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/1d54db99-c0dc-49fd-b5ae-f1b51fad61f2-p0obbf.jpg",
@@ -13,11 +14,21 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
-    <div className="grid max-w-[800px] grid-cols-12 gap-4 m-4">
-      {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-        <div key={image.id} className="col-span-3 max-h-32 max-w-32">
+    <div className="grid max-w-[1200px] grid-cols-12 gap-4 m-4">
+      {posts.map((post) => (
+        <div key={post.id} className="col-span-3 max-h-32 max-w-32">
+          {post.name}
+        </div>
+      
+      ))}
+      {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        <div key={image.id + "-" + index} className="col-span-3 max-h-32 max-w-32">
           <Image src={image.url} alt="alt" width={128} height={128} />
         </div>
       ))}
